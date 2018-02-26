b5be22c673d46d68c456ebc298a1f94368a98b17
bobwis <bob.wisdom@gmail.com>
2018-02-26 12:10:34 +1000

    minor changes

--- Src/ethernetif.c
+++ Src/ethernetif.c
@@ -249,7 +249,7 @@ static void low_level_init(struct netif *netif)
   MACAddr[2] = 0xE1;
   MACAddr[3] = 0x00;
   MACAddr[4] = 0xAA;
-  MACAddr[5] = 0x01;
+  MACAddr[5] = 0x02;
   heth.Init.MACAddr = &MACAddr[0];
   heth.Init.RxMode = ETH_RXINTERRUPT_MODE;
   heth.Init.ChecksumMode = ETH_CHECKSUM_BY_HARDWARE;
